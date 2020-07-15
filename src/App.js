6f87514641ac3b17ecd5201b6c19ede05f5556ae
“Евгений <sishnenko86@mail.ru>
2020-07-16 00:12:16 +0300

    первая неделя

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Contacts from "./pages/Contacts/Contacts";
+import About from "./pages/About/About";
+import Catalog from "./pages/Catalog/Catalog";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Page404 from "./Components/Page404/Page404";
+import Banner from "./Components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Banner />
+        <Switch>
+          <Route path="/ " component={Home} />
+          <Route path="/contacts.html" component={Contacts} />
+          <Route path="/about.html" component={About} />
+          <Route path="/catalog.html" component={Catalog} />
+          <Route component={Page404} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Page404/Page404.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Page404() {
+  return (
+    <main className="container">
+      <div className="row">
+        <div className="col">
+          <section className="top-sales">
+            <h2 className="text-center">Страница не найдена</h2>
+            <p>Извините, такая страница не найдена!</p>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Page404;
